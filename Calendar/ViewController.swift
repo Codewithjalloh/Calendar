3a2c0f6a3a1b7e3d78bbdcbd64852d795a4d4122
info@codewithjalloh.com <supremejalloh>
2016-07-17 13:10:27 +0100

    properties and outlet connected

--- Calendar/ViewController.swift
+++ Calendar/ViewController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import EventKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // properties
+    var eventStore: EKEventStore!
+    var calendar: EKCalendar!
+    
+    // outlets
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var eventCalenda: UITextField!
+    @IBOutlet weak var titleEvent: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
