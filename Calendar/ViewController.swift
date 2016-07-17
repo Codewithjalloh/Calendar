04f9cfae7367a327be6f04168591d5814518b940
info@codewithjalloh.com <supremejalloh>
2016-07-17 13:14:10 +0100

    save calendar function added

--- Calendar/ViewController.swift
+++ Calendar/ViewController.swift
@@ -20,6 +20,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextView!
     @IBOutlet weak var eventCalenda: UITextField!
     @IBOutlet weak var titleEvent: UITextField!
+    
+    
+    
+    @IBAction func saveCal(sender: UIButton) {
+        let cal = EKCalendar(forEntityType: .Event, eventStore: eventStore)
+        eventStore.requestAccessToEntityType(.Event, completion: { (granted, error) in
+        
+            if (granted == false ) {
+                print("Access Denied")
+            } else {
+                var auxiliar = self.eventStore.sources
+                cal.source = auxiliar[0]
+                cal.title = self.textField.text!
+                print(cal.title)
+                
+                try! self.eventStore.saveCalendar(cal, commit: true)
+            }
+        })
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
